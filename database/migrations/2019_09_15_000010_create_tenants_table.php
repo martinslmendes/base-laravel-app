10ebb1162cf81f0bf1a4f15df6c9321fde01faa5
Lucas Mendes Martins <martinslmendes@gmail.com>
2026-05-20 14:00:45 -0300

    build: installed and configured stancl/tenancy

---
+++ database/migrations/2019_09_15_000010_create_tenants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(table: 'tenants', callback: function (Blueprint $table) {
+            $table->string(column: 'id')->primary();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->json(column: 'data')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
