db5b3c2dfc7c5cb780518991ab186165b4ae079e
Lucas Mendes Martins <martinslmendes@gmail.com>
2026-05-21 09:32:56 -0300

    feat: attach user to tenant and create personal team

--- app/Models/TenantPivot.php
+++ app/Models/TenantPivot.php
@@ -3,11 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Stancl\Tenancy\Database\Models\TenantPivot as BaseTenantPivot;
 
-class TenantPivot extends Pivot
+class TenantPivot extends BaseTenantPivot
 {
     use HasUuids;
 
     protected $primaryKey = 'uuid';
+    protected $table = 'tenant_users';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(
+            related: User::class,
+            foreignKey: 'user_id',
+            ownerKey: 'uuid',
+            relation: 'users'
+        );
+    }
+
+    public function tenant(): BelongsTo
+    {
+        return $this->belongsTo(
+            related: Tenant::class,
+            foreignKey: 'tenant_id',
+            ownerKey: 'id',
+            relation: 'tenants'
+        );
+    }
 }
